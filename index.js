0c342eaf5d5aa1b5ede380c85a62573e9f226066
irriz-jayy <jayfelix789@gmail.com>
2022-11-17 22:07:19 +0300

    Last function issues

---
+++ index.js
@@ -0,0 +1,39 @@
+function shout(string) {
+  return string;
+}
+
+function shout(string) {
+  return string.toUpperCase();
+}
+
+function whisper(string) {
+  return string;
+}
+
+function whisper(string) {
+  return string.toLowerCase();
+}
+
+function logShout(string) {
+  console.log(string);
+}
+
+function logShout(string) {
+  console.log(string.toUpperCase());
+}
+
+function logWhisper(string) {
+  console.log(string);
+}
+
+function logWhisper(string) {
+  console.log(string.toLowerCase());
+}
+
+if (string === string.toLowerCase) {
+  return "h";
+} else if (string === string.toUpperCase) {
+  return "YES INDEED!";
+} else if (string === "Let's have dinner together") {
+  return "I would love to";
+}
